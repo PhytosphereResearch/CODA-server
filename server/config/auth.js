20604d0d8f02f206a56b60d473ac8f564bed6a8f
Phytosphere <lizbernhardt@gmail.com>
2024-11-26 15:41:43 -0800

    update auth code to function

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,24 +1,15 @@
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+const { auth } = require("express-oauth2-jwt-bearer");
+const dotenv = require("dotenv");
 
-// Authentication middleware. When used, the
-// access token must exist and be verified against
-// the Auth0 JSON Web Key Set
-const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the singing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: process.env.JWKS_URI,
-  }),
+dotenv.config();
 
-  // Validate the audience and the issuer.
-  audience: process.env.AUDIENCE,
-  issuer: process.env.ISSUER,
-  algorithms: ['RS256'],
+const validateAccessToken = auth({
+  issuerBaseURL: `${process.env.AUTH0_DOMAIN}`,
+  audience: process.env.AUTH0_AUDIENCE,
+  tokenSigningAlg: 'RS256',
 });
 
-module.exports = checkJwt;
+module.exports = {
+  validateAccessToken,
+};
+
