e561caa856657859d34afe74263c334be7ba1593
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2025-01-31 17:57:44 -0800

    Attempt to reflect origin

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: ture }));
 
 middleware(app, express);
 routes(app, express); //new routes
