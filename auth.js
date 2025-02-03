2b471ecc459ae64fe4b029ed0479873c02002f7e
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2025-02-02 21:31:54 -0800

    bugfix: fail on error differently (#37)
    * have context fail on error

--- auth.js
+++ auth.js
@@ -21,6 +21,6 @@ export const handler = async (event, context) => {
     context.succeed({ isAuthorized: true });
   } catch (e) {
     console.error("Error->\n", e);
-    context.fail({ isAuthorized: false });
+    context.fail("Unauthorized");
   }
 };
