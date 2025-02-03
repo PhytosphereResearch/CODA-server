b830175515d14bd6876584e65cd3bf21eec06564
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2025-02-02 20:56:46 -0800

    attempt to fix authorization

---
+++ auth.js
@@ -0,0 +1,26 @@
+import { jwtVerify } from "jose";
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const JWKS = createRemoteJWKSet(
+  new URL(`https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`)
+);
+
+const authenticate = ({ authorizationToken }) => {
+  return jwtVerify(jauthorizationTokenwt, JWKS, {
+    issuer: `${process.env.AUTH0_DOMAIN}/`,
+    audience: process.env.AUTH0_AUDIENCE,
+  });
+};
+
+export const handler = async (event, context) => {
+  try {
+    await authenticate(event);
+    context.succeed({ isAuthorized: true });
+  } catch (e) {
+    console.error("Error->\n", e);
+    context.succeed({ isAuthorized: false });
+  }
+};
