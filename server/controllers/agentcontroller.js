9929e8bbf380cd16bb90560bf395f6dc9f9b22c0
Phytosphere <lizbernhardt@gmail.com>
2025-05-15 16:30:02 -0700

    changes to allow edit trail for edit agents

--- server/controllers/agentcontroller.js
+++ server/controllers/agentcontroller.js
@@ -17,7 +17,7 @@ const getDistinct = (colName, colAlias) => db.agents.findAll({
   raw: true,
 })
   .then(res => res.map(entry => entry[colAlias]).sort().filter(entry => entry));
- 
+
 
 module.exports = {
 
@@ -65,57 +65,53 @@ module.exports = {
   },
 
   async post(request, response) {
-    
+
     //things needed to make a record in edit_trails
     let currentDate = new Date();
-    date=currentDate.toLocaleString();
-    // console.log(date);
-    const user_id = request.body.userName;
-    table_record_id = request.body.agent.id;
-    record = request.body.agent;
-    new_recordStr = JSON.stringify({ record });
-    console.log("new_recordStr", new_recordStr, "table_record_id", table_record_id, "user_id", user_id, "date", date, "currentDate", currentDate);
+    const { userName, agent } = request.body;
+    const agentId = agent.id;
 
-    const trail = await db.edit_trails.create({ 
-      user_id: user_id,
-       table_name: 'agents', 
-       table_record_id: table_record_id, 
-       new_record: new_recordStr, 
-      date_time: currentDate
-      });
-  
     const allParams = request.body.agent;
-    // console.log("allParams", allParams);
-    
     if (allParams.id) {
       const { id } = allParams;
-    db.agents.findOne({ where: { id } })
-       
-    .then((record) => {
-      // console.log("record", record);
-      record.update(allParams)
-        .then((agt) => {
-          response.status(201).json(agt);
-          // console.log("agt", agt);
+      const trail = await db.edit_trails.create({
+        user_id: userName,
+        table_name: 'agents',
+        table_record_id: agentId,
+        new_record: JSON.stringify(agent),
+        date_time: currentDate
+      });
+      db.agents.findOne({ where: { id } })
+        .then((record) => {
+          record.update(allParams)
+            .then((agt) => {
+              response.status(201).json(agt);
+            });
         });
-    });
-} else {
-  const { agent, synonym } = allParams;
+    } else {//if new agent created
+      const { agent, synonym } = allParams;
+      const newAgent = await db.agents.create(agent)
+      const agentID = newAgent.dataValues.id;
 
-      record = allParams;
-      new_recordStr = JSON.stringify({ record });
-      console.log("new_recordStr", new_recordStr, "table_record_id", table_record_id, "user_id", user_id, "date", date, "currentDate", currentDate);
-
-
-  db.agents.create(agent)
-    .then((res) => {
-      const agentID = res.dataValues.id;
       synonym.agentId = agentID;
-      db.synonyms.create(synonym)
-        .then((agt) => {
-          response.status(201).json(agt);
-        });
-    });
-}
+      const agt = await db.synonyms.create(synonym)
+      await db.edit_trails.create({
+        user_id: userName,
+        table_name: 'agents',
+        table_record_id: agentID,
+        new_record: JSON.stringify(agent),
+        date_time: currentDate
+      });
+
+      await db.edit_trails.create({
+        user_id: userName,
+        table_name: 'synonyms',
+        table_record_id: agt.dataValues.id,
+        new_record: JSON.stringify(synonym),
+        date_time: currentDate
+      });
+      response.status(201).json(agt);
+
+    }
   },
 };
