00ff04dc2ac3c64d3c911adaf31c1dbe67aa9ff5
Phytosphere <lizbernhardt@gmail.com>
2025-06-16 22:52:31 -0700

    this works for add and update oaks table

--- server/controllers/oakcontroller.js
+++ server/controllers/oakcontroller.js
@@ -1,6 +1,6 @@
 const db = require('../db');
 const helper = require('./helper');
-
+const Sequelize = require('sequelize');
 module.exports = {
 
   async getAllOaks(request, response) {
@@ -17,6 +17,7 @@ module.exports = {
 
   async getOaksByAgent(request, response) {
     const agentId = request.params.agentId;
+    console.log("agentId", agentId);
     try {
       const data = await db.oaks.findAll({
         attributes: ['id', 'genus', 'species', 'subSpecies'],
@@ -31,6 +32,7 @@ module.exports = {
 
   async getOakById(request, response) {
     const id = request.params.id;
+    console.log("id", id);
     try {
       const oak = await db.oaks.findOne({ where: { id }, logging: console.log })
       response.status(200).json(oak);
@@ -42,9 +44,20 @@ module.exports = {
 
   async addOak(request, response) { // post a new oak record or update
     try {
-      const params = request.body;
-      if (params.id) {
-        const id = params.id;
+      const params = request.body.oak;
+      const id = params.id;
+      const { userName } = request.body;
+
+      //to create record in auditlogs for editing oak
+      if (id) {
+        const trail = await db.auditLogs.create({//side code to make a record in auditLogs
+          user_id: userName,
+          table_name: 'oaks',
+          table_record_id: id,
+          action: 'update',
+          new_record: JSON.stringify(params),
+        })
+
         const oak = await db.oaks.findOne({ where: { id } })
           .then((record) => {
             record.update(params)
@@ -55,6 +68,13 @@ module.exports = {
       } else {
         db.oaks.create(params)
           .then((oak) => {
+            db.auditLogs.create({//side code to make a record in auditLogs for add oak
+              user_id: userName,
+              table_name: 'oaks',
+              table_record_id: oak.id,
+              action: 'create',
+              new_record: JSON.stringify(params),
+            })
             response.status(201).json(oak);
           })
       }
