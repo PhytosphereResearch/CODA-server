7cfff32450aa17488034b44d3f534b96799ad615
Phytosphere <lizbernhardt@gmail.com>
2025-06-20 14:19:24 -0700

    refactor audit logic

--- server/controllers/bibcontroller.js
+++ server/controllers/bibcontroller.js
@@ -14,37 +14,32 @@ module.exports = {
   },
 
   async addOrUpdate(request, response) {
-    console.log("request", request.body);
-    const { reference, userName } = request.body;
-    console.log("user", userName);
-    if (reference.id) {
-      const { id } = reference;
+    try {
+      const { reference, userName } = request.body;
+      const isUpdate = !!reference.id
+      let res;
 
-      await db.auditLogs.create({//side code to make a record in auditLogs
-        user_id: userName,
-        table_name: 'bibs',
-        table_record_id: id,
-        action: 'update',
-        new_record: JSON.stringify(reference),
-      })
-
-      const record = await db.bibs.findOne({ where: { id } })
-      const res = await record.update(reference)
-      return response.status(201).json(res);
-
-    } else {
-      const res = await db.bibs.create(reference)
+      if (isUpdate) {
+        const { id } = reference;
+        const record = await db.bibs.findOne({ where: { id } })
+        res = await record.update(reference)
+      } else {
+        res = await db.bibs.create(reference)
+      }
 
       await db.auditLogs.create({//side code to make a record in auditLogs
         user_id: userName,
         table_name: 'bibs',
         table_record_id: res.id,
-        action: 'update',
+        action: isUpdate ? 'update' : 'create',
         new_record: JSON.stringify(reference),
       })
 
       return response.status(201).json(res);
+    } catch (error) {
+      return response.status(500).json(error);
     }
+
   },
 
 };
