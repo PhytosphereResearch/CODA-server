440c5100f4c58fd963ad68f95c12a41da2087444
Phytosphere <lizbernhardt@gmail.com>
2025-06-29 22:03:37 -0700

    refactor addSynonym and add audit logging

--- server/controllers/syncontroller.js
+++ server/controllers/syncontroller.js
@@ -3,70 +3,82 @@ const helper = require('./helper');
 
 
 module.exports = {
- async getAllSynonyms(request, response) { // retrieve synonym records
-  try{
-    const data = await db.synonyms.findAll({
-    include: { model: db.agents, 
-      attributes: ['commonName'] }
+  async getAllSynonyms(request, response) { // retrieve synonym records
+    try {
+      const data = await db.synonyms.findAll({
+        include: {
+          model: db.agents,
+          attributes: ['commonName']
+        }
+      });
+      response.status(200).json(data);
     }
-  );
-    response.status(200).json(data);
-  }
-  catch (err) {
-    helper.handleError(response)(err);
-  }
-},
+    catch (err) {
+      helper.handleError(response)(err);
+    }
+  },
 
-  // TODO: refactor this
-  addSynonym(request, response) { // add a new synonym
-    const params = request.body;
-    if (params.id) {
-      if (params.isPrimary === 1) { // changes other primary flags to false
-        db.synonyms.update(
+  async addSynonym(request, response) { // add a new synonym
+    try {
+      const { synonym, userName } = request.body;
+      let res;
+      let msg;
+      let isUpdate;
+
+      if (synonym.id) {//for editing an existing synonym
+
+        if (synonym.isPrimary === 1) { // changes other primary flags to false
+          res = await db.synonyms.update(
+            { isPrimary: 0 },
+            {
+              fields: ['isPrimary'],
+              where: { agentId: synonym.agentId },
+            }
+          )
+
+          const { id } = synonym;
+          const record = await db.synonyms.findOne({ where: { id } })
+          res = await record.update(synonym)
+          msg = 'Synonym updated';
+          isUpdate = true;
+        }
+        else {//if editing fields other than is primary
+          const { id } = synonym;
+          const record = await db.synonyms.findOne({ where: { id } })
+          res = await record.update(synonym)
+           msg = 'Synonym updated';
+           isUpdate = true;
+        };
+      } 
+      else if (synonym.isPrimary === 1) { // for new synonym params.id undefined, changes other primary flags to false 
+        res = await db.synonyms.update(
           { isPrimary: 0 },
           {
             fields: ['isPrimary'],
-            where: { agentId: params.agentId },
+            where: { agentId: synonym.agentId },
           }
-        ).then(() => {
-          const { id } = params;
-          db.synonyms.findOne({ where: { id } })
-            .then((record) => {
-              record.update(params)
-                .then(() => {
-                  response.status('201').json({ message: 'Synonym updated' });
-                });
-            });
-        });
-      } else {
-        const { id } = params;
-        db.synonyms.findOne({ where: { id } })
-          .then((record) => {
-            record.update(params)
-              .then(() => {
-                response.status('201').json({ message: 'Synonym updated' });
-              });
-          });
+        )
+        res = await db.synonyms.create(synonym)
+        msg = 'Your synonym has been added';
+        isUpdate = false;
+      } 
+      else {//if params.isPrimary=0 for a new synonym not in database
+        res = await db.synonyms.create(synonym)
+        msg = 'Your synonym has been added';
+        isUpdate = false;
       }
-    } else if (params.isPrimary === 1) { // changes other primary flags to false
-      db.synonyms.update(
-        { isPrimary: 0 },
-        {
-          fields: ['isPrimary'],
-          where: { agentId: params.agentId },
-        }
-      )
-        .then(() => {
-          db.synonyms.create(params)
-            .then(() => {
-              response.status('201').json({ message: 'Your synonym has been added' });
-            });
-        });
-    } else {
-      db.synonyms.create(params)
-        .then(() => {
-          response.status('201').json({ message: 'Your synonym has been added' });
-        });
+
+      await db.auditLogs.create({//side code to make a record in auditLogs
+        user_id: userName,
+        table_name: 'synonyms',
+        table_record_id: res.id,
+        action: isUpdate ? 'update' : 'create',
+        new_record: JSON.stringify(synonym),
+      })
+
+      return response.status(201).json({ res, msg });
+    } catch (error) {
+      return response.status(500).json(error);
     }
   },
 };
