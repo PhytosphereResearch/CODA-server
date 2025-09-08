c26f23da4e8a47381a3c204086fee0e7414d92f1
Phytosphere <30961253+Phytosphere@users.noreply.github.com>
2025-09-08 16:21:07 -0700

    Add audit log results to interaction page (#86)
    * change record history to interactions
    
    * corrected field value
    
    * minor reformat remove empty line
    
    * add a constant for text string
    
    * add a constant for string
    
    * add const editedHiTables for string
    
    * add const editedHiTables for string
    
    * add back in accidentally deleted line

--- server/controllers/constants.js
+++ server/controllers/constants.js
@@ -1,1 +1,1 @@
-module.exports ={UPDATE: 'update', CREATE: 'create'};
+module.exports = { UPDATE: 'update', CREATE: 'create', editedHiTables: 'hostinteractions or related'};
--- server/controllers/hicontroller.js
+++ server/controllers/hicontroller.js
@@ -2,7 +2,8 @@ const db = require('../db');
 const helper = require('./helper');
 const Sequelize = require('sequelize');
 const uniq = require('lodash.uniq');
-const { UPDATE, CREATE } = require('./constants');
+const { UPDATE, CREATE, editedHiTables } = require('./constants');
+const auditController = require('./auditcontroller');
 
 const { Op } = Sequelize;
 
@@ -123,7 +124,9 @@ module.exports = {
         ],
         where: { id: hiId },
       });
-      response.status(200).json(data);
+      const auditRecords = await auditController.getAuditRecords(hiId, editedHiTables);
+      const dataParse = JSON.parse(JSON.stringify(data));
+      response.status(200).json({ ...dataParse, auditRecords });
     }
     catch (err) {
       helper.handleError(response)(err);
@@ -196,7 +199,7 @@ module.exports = {
         updateHiReferences(res, hi.bibs),
       ].concat(hiSymptomList.map(hiSymptom => updateHiSymptom(hiSymptom))));
 
-      let rec={};
+      let rec = {};
       if (isUpdate) {
         let { dataValues, isNewRecord } = res;
         rec = { dataValues, isNewRecord, hiLocations, hiReferences, hiSymptoms };
@@ -207,7 +210,7 @@ module.exports = {
 
       await db.auditLogs.create({//side code to make a record in auditLogs
         user_id: userName,
-        table_name: 'hostinteractions or related',
+        table_name: editedHiTables,
         table_record_id: isUpdate ? hi.id : res.id,
         action: isUpdate ? UPDATE : CREATE,
         new_record: JSON.stringify(rec),
